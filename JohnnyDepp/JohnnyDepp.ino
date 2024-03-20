10fde768b601444f70beda71a960401df65bf4bf
Mihael Druzeta <144025453+MichaelDruzeta@users.noreply.github.com>
2024-03-20 13:26:01 +0100

    Add files via upload

--- JohnnyDepp/JohnnyDepp.ino
+++ JohnnyDepp/JohnnyDepp.ino
@@ -1,4 +1,7 @@
-#include <QYFSensors.h>
+#include <Servo.h>
+Servo gripper;
+int openPos = 48;
+int closePos = 125;
 
 // Defining the pins connected to the motor driver module
 const int motorLeftF = 3; // Left motor forward
@@ -12,9 +15,13 @@ const int rotaryRight = 6; // Right rotary sensor
 int ticksLeft;
 int ticksRight;
 
+// Defining pins for echo and trigger of HC-SR04
+const int echoPin = 9;
+const int trigPin = 8;
+long duration; // Variable to store the time taken to the pulse to reach the receiver
+long distance; // Variable to store the distance calculated using the distance formula
+
 // Integers for adjusting the speed
-int speedRight;
-int speedLeft;
 int valueRight;
 int valueLeft;
 
@@ -47,14 +54,17 @@ void setup(){
     }
   }
 
+
 void loop(){
   readLineSensors();
-  int sensorValue = analogRead(LS1);
-  delay(500);
+  Serial.println(LS2);
+  Serial.println(LS3);
+  Serial.println(LS4);
+  delay(200);
 
-  if(startRace){
-      followLine();
-    }  
+//  if(startRace){
+  followLine();
+    
 }
 
 // ---------------------------------------------------- SETUPS 
@@ -97,19 +107,20 @@ void setupRotarySensor(){
 }
 
 boolean detectedFinishSquare(){
-  if (LS1 > 300 && LS2 > 300 && LS3 > 300 && LS4 > 300 && LS5 > 300 && LS6 > 300 && LS7 > 300 && LS8 > 300){
+  if (LS1 > 900 && LS2 > 900 && LS3 > 900 && LS4 > 900 && LS5 > 900 && LS6 > 900 && LS7 > 900 && LS8 > 900){
     return true;
   }
 }
 
-// ---------------------------------------------------- MOTOR FUNCTION
+void openGripper(){
+    gripper.write(openPos);
+  }
 
-void idle(){
-  digitalWrite(motorLeftF, LOW);
-  digitalWrite(motorRightF, LOW);
-  digitalWrite(motorLeftB, LOW);
-  digitalWrite(motorRightB, LOW);
-}
+  void closeGripper(){
+    gripper.write(closePos);
+  }
+
+// ---------------------------------------------------- MOTOR FUNCTION
 
 void turnLeft(int ticksRemaining, int speed){
   ticksLeft = 0;
@@ -143,9 +154,9 @@ void backwards(){
 }
 
 void forward(){
-  analogWrite(motorLeftF, 200);
+  analogWrite(motorLeftF, 150);
   digitalWrite(motorLeftB, LOW);
-  analogWrite(motorRightF, 200);
+  analogWrite(motorRightF, 150);
   digitalWrite(motorRightB, LOW);
 }
 
@@ -159,28 +170,18 @@ void distanceForward(int ticksRemaining, int speed){
 }
 
 void adjustLeft(){  // Adjusts the left motor
-  analogWrite(motorLeftF, 255);
-  analogWrite(motorRightF, speedRight);
+  analogWrite(motorLeftF, 170);
+  analogWrite(motorRightF, 150);
 }
 
 void adjustRight(){  // Adjusts the right motor
-  analogWrite(motorRightF, 255);
-  analogWrite(motorLeftF, speedLeft);
-}
-
-void tickLeft(){
-  ticksLeft++;
-  interrupts();
-}
-
-void tickRight(){
-  ticksRight++;
-  interrupts();
+  analogWrite(motorRightF, 170);
+  analogWrite(motorLeftF, 150);
 }
 
 void followLine(){
   readLineSensors();
-    if (LS2 > 300 || LS3 > 300 || LS4 > 300 || LS5 > 300 || LS6 > 300 || LS7 > 300){
+    if (LS2 > 970 || LS3 > 960 || LS4 > 960 || LS5 > 970 || LS6 > 980 || LS7 > 980){
       int valueLeft = (LS5 + LS6 + LS7) / 3;
       int valueRight = (LS2 + LS3 + LS4) / 3;
 
@@ -195,4 +196,88 @@ void followLine(){
     adjustLeft(); 
     // vice versa if the right value is bigger, go slightly to the left
   }
+}
+
+// ---------------------------------------------------- AVOID OBJECT FUNCTION
+
+void readDistance(){
+  //send a 10ms pulse to HC-SR04
+  digitalWrite(trigPin, LOW); //initialize trigPin on low
+  delayMicroseconds(2); // wait 2ms before sending pulse
+  digitalWrite(trigPin,HIGH); //send pulse to trigPin to send UltraSonic wave for detection
+  delayMicroseconds(10); //keep trigPin HIGH for 10ms
+  digitalWrite(trigPin,LOW); //stop sending pulse
+  //calculating distance
+  duration = pulseIn(echoPin, HIGH); //storing how much time it took to get the pulse back
+  distance = duration * 0.034 / 2; //calculating distance based on time from pulseIn
+  delay(500); // waiting before taking another measurement
+}
+
+void avoidObject(){
+    boolean turningLeft = true;
+    boolean turningRight = false;
+    boolean changedPosition1 = false;
+    boolean changedPosition2 = false;
+
+    if(turningLeft){
+      idle(); //stopping the motors so the robot will not move
+      turnLeft(30, 200);// turning left to avoid object
+      turningLeft = false;
+      changedPosition1 = true;
+    }
+    if(changedPosition1){
+      idle(); //stopping the motors so the robot will not move
+      distanceForward(50, 200); // going forward - on the lateral side of the object
+      changedPosition1 = false;
+      turningRight = true;
+    }
+    if(turningRight){
+      idle(); //stopping the motors so the robot will not move
+      turnRight(30, 200); //moving right so robot will be headed back for the line
+      delay(900);
+      changedPosition2 = true;
+      turningRight = false;
+    }
+    if(changedPosition2){
+      idle(); // stopping the motor so the robot will not move
+      distanceForward(50, 200); // going forward to get to the line -- object avoided
+      delay(1000);
+      changedPosition1 = false;
+      changedPosition2 = false;
+      turningRight = false;
+      turningLeft = false;
+    }
+  }
+
+void getDistance(){
+  //send a 10ms pulse to HC-SR04
+  digitalWrite(trigPin, LOW); //initialize trigPin on low
+  delayMicroseconds(2); // wait 2ms before sending pulse
+  digitalWrite(trigPin,HIGH); //send pulse to trigPin to send UltraSonic wave for detection
+  delayMicroseconds(10); //keep trigPin HIGH for 10ms
+  digitalWrite(trigPin,LOW); //stop sending pulse
+  //calculating distance
+  duration = pulseIn(echoPin, HIGH); //storing how much time it took to get the pulse back
+  distance = duration * 0.034 / 2; //calculating distance based on time from pulseIn
+  Serial.println(distance);
+   // waiting before taking another measurement
+}
+
+void idle(){
+  analogWrite(motorLeftF, LOW);
+  analogWrite(motorRightF, LOW);
+    digitalWrite(motorLeftB, LOW);
+  digitalWrite(motorRightB, LOW);
+}
+
+void tickLeft(){
+  noInterrupts();
+  ticksLeft++;
+  interrupts();
+}
+
+void tickRight(){
+  noInterrupts();
+  ticksRight++;
+  interrupts();
 }
